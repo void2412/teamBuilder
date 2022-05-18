053761cc18fec3a2239960bc711ee5e9d9763474
Chi Hieu Nguyen <void2412@gmail.com>
2022-05-18 21:41:14 +0930

    added skeleton for index.js, finished getManagerDetails function

--- index.js
+++ index.js
@@ -2,3 +2,48 @@ const engineer = require('./lib/engineer')
 const intern =  require('./lib/intern')
 const manager = require('./lib/manager')
 const generatePage = require('./src/pageGenerator')
+const  inquirer = require('inquirer')
+
+async function getEmployeeList(){
+	var employeeList = []
+
+}
+
+async function getManagerDetails(){
+	var result = await inquirer.prompt([{
+		name:'name',
+		type:'input',
+		message: `What is the team manager's name?`	
+	},
+	{
+		name:'id',
+		type:'input',
+		message: `What is the team manager's id?`	
+	},
+	{
+		name:'email',
+		type:'input',
+		message: `What is the team manager's email?`	
+	},
+	{
+		name:'officeNumber',
+		type:'input',
+		message: `What is the team manager's office number?`	
+	}
+])
+	return result
+}
+
+async function getEngineerDetails(){
+
+}
+
+async function getInternDetails(){
+
+}
+
+async function getAddMemberChoice(){
+
+}
+
+getManagerDetails()
