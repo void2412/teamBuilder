2ac3e85716b0c50e8568dc83c67def3e2929711b
Chi Hieu Nguyen <void2412@gmail.com>
2022-05-18 20:12:06 +0930

    add skeleton functions to pageGenerator, add import for index

---
+++ index.js
@@ -0,0 +1,4 @@
+const engineer = require('./lib/engineer')
+const intern =  require('./lib/intern')
+const manager = require('./lib/manager')
+const generatePage = require('./src/pageGenerator')
---
+++ src/pageGenerator.js
@@ -0,0 +1,21 @@
+function getGithubLink(username){
+
+}
+
+function renderGithub(username){
+
+}
+
+function renderEmail(email){
+
+}
+
+function generateCard(employee){
+
+}
+
+function generatePage(data){
+
+}
+
+module.exports = generatePage
