0aa5c33574c47fad7c1a23c83dd9314e7659cf94
Chi Hieu Nguyen <void2412@gmail.com>
2022-05-18 21:46:53 +0930

    finished remaining function except getEmployeeList

--- index.js
+++ index.js
@@ -35,15 +35,67 @@ async function getManagerDetails(){
 }
 
 async function getEngineerDetails(){
-
+	var result = await inquirer.prompt([{
+		name:'name',
+		type:'input',
+		message: `What is the engineer's name?`	
+	},
+	{
+		name:'id',
+		type:'input',
+		message: `What is the engineer's id?`	
+	},
+	{
+		name:'email',
+		type:'input',
+		message: `What is the engineer's email?`	
+	},
+	{
+		name:'github',
+		type:'input',
+		message: `What is the engineer's Github username?`	
+	}
+])
+	return result
 }
 
 async function getInternDetails(){
-
+	var result = await inquirer.prompt([{
+		name:'name',
+		type:'input',
+		message: `What is the intern's name?`	
+	},
+	{
+		name:'id',
+		type:'input',
+		message: `What is the intern's id?`	
+	},
+	{
+		name:'email',
+		type:'input',
+		message: `What is the intern's email?`	
+	},
+	{
+		name:'school',
+		type:'input',
+		message: `What is the intern's school?`	
+	}
+])
+	return result
 }
 
 async function getAddMemberChoice(){
+	var result = await inquirer.prompt([{
+		name:'memberChoice',
+		type:'list',
+		choice:['Engineer', 'Intern', 'Finish adding member'],
+		message: 'Which type of team member would you like to add?'
+	}])
 
+	if (result.memberChoice == 'Finish adding member')
+		return false
+	
+	return result
 }
 
 getManagerDetails()
